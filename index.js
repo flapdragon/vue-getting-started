b014d9839b736dcd059b2c1c37f4fd97502cf8da
David Dean <vermicious.knid.d@gmail.com>
2018-02-04 15:00:07 -0700

    Event handler.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var growler = new Vue({
   el: '#growler',
   data: {
     appName: 'Growler',
-    query: ''
+    query: 'the event is easier to see with text'
+  },
+  methods: {
+    executeSearch:function() {
+      console.log(this.query)
+    }
   }
 })
