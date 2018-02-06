a16e1330f6a3e212c5893e8660e1ee0e4da6f9df
David Dean <vermicious.knid.d@gmail.com>
2018-02-05 22:43:03 -0700

    Stop modifier.

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var growler = new Vue({
     query: ''
   },
   methods: {
+    grandparentClick:function(event) {
+      console.log('method: grandparentClick')
+    },
+    parentClick:function(event) {
+      console.log('method: parentClick')
+    },
     executeSearch:function(event) {
-      if (this.query) {
-        console.log('submit')
-        document.forms[0].submit()
-      }
-      else {
-        alert('Show me da qwaery')
-      }
+      console.log('method: executeSearch')
     }
   }
 })
